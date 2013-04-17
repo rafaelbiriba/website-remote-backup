7b580dbf8fcce69257ca0a43ac1f1b1689fe8956
Rafael Biriba <biribarj@gmail.com>
2013-04-17 19:02:58 -0300

    melhorias na forma de criar as senhas do banco mysql

--- lib/recipes/mysql_stuffs.rb
+++ lib/recipes/mysql_stuffs.rb
@@ -1,14 +1,23 @@
 desc "Create mysqldump password file in server"
 namespace :mysql do
   task :prepare do
-    puts "Preparando para criar o arquivo de senhas do mysql..."
-    server_name = capture("echo $CAPISTRANO:HOST$").strip
-    puts "Configuração do mysql do server #{server_name.yellow}:"
-    db_user = Capistrano::CLI.ui.ask("Usuário do mysql: ")
-    db_password = Capistrano::CLI.password_prompt("Senha do mysql: ")
-    print "Salvando no server: "
+    print "Verificando hostname do servidor: "
     start_spinner
-    run("cd ~/ && echo -e \"[mysqldump]\\nuser=#{db_user}\\npassword=#{db_password}\\n\\n[client]\\nuser=#{db_user}\\npassword=#{db_password}\" > .my.cnf && chmod 600 .my.cnf")
+    server_name = capture("echo $CAPISTRANO:HOST$").strip
+    stop_spinner
+    puts "OK".green
+    servers = Server.select{ |s| s.database.host == server_name && s.database.enable_database_backup == true}
+    mysqlpassfile = ""
+    puts "Preparando para criar o arquivo de senhas do mysql no host #{server_name.yellow}..."
+    servers.each do |server|
+      puts "Informações do database #{server.database.database_name.yellow}"
+      db_user = Capistrano::CLI.ui.ask("Usuário do mysql: ")
+      db_password = Capistrano::CLI.password_prompt("Senha do mysql: ")
+      mysqlpassfile << "[mysqldump]\\nuser=#{db_user}\\npassword=#{db_password}\\n\\n[client]\\nuser=#{db_user}\\npassword=#{db_password}\\n\\n"
+    end
+    print "Salvando arquivo no server: "
+    start_spinner
+    run("cd ~/ && echo -e \"#{mysqlpassfile}\" > .my.cnf && chmod 600 .my.cnf")
     stop_spinner
     puts "OK".green
   end
