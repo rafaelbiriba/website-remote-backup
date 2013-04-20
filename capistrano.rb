0341f58e9ddb1e684e1194c4758958ac00acaf52
Rafael Biriba <biribarj@gmail.com>
2013-04-19 22:04:54 -0300

    mais melhorias na criacao do user do mysql

--- capistrano.rb
+++ capistrano.rb
@@ -1,11 +1,21 @@
 load "config/initializer"
-
-set :user, Server.first.connection.ssh_user
-ssh_password = Server.first.connection.ssh_password
-set :password, ssh_password unless ssh_password.nil?
 set :use_sudo, false
-ssh_options[:port] = Server.first.connection.ssh_port
-
 default_run_options[:pty] = true
+default_run_options[:max_hosts] = 1
 
-role :app, Server.first.connection.host
+Server.each do |s|
+  host = s.connection.host
+  next unless find_servers.select{ |s| s.host == host }.empty?
+  
+  ssh_password = s.connection.ssh_password
+  
+  params = {
+    user: s.connection.ssh_user,
+    ssh_options: { port: s.connection.ssh_port }
+  }
+  
+  params.merge!( password: ssh_password ) unless ssh_password.nil?
+  
+  server host, :db, params unless s.database.nil?
+  server host, :app, params unless s.website.nil?
+end
--- lib/recipes/mysql_stuffs.rb
+++ lib/recipes/mysql_stuffs.rb
@@ -1,38 +1,45 @@
+require 'digest/md5'
+
 desc "Create mysqldump password file in server"
 namespace :mysql do
-  task :prepare do
-    print "Verificando hostname do servidor: "
-    start_spinner
-    server_name = capture("echo $CAPISTRANO:HOST$").strip
-    stop_spinner
-    puts "OK".green
-    servers = Server.select{ |s| s.database.host == server_name }
-    mysqlpassfile_content = ""
-    puts "Preparando para criar o arquivo de senhas do mysql no host #{server_name.yellow}..."
-    databases = []
-    servers.each_with_index do |server, i|
-      databases << server.database.database_name
-      puts "Informações do database #{server.database.database_name.yellow}"
-      db_user = Capistrano::CLI.ui.ask("Usuário do mysql: ")
-      db_password = Capistrano::CLI.password_prompt("Senha do mysql: ")
-      mysqlpassfile_content << "[mysqldump#{i}]\\nuser=#{db_user}\\npassword=#{db_password}\\n\\n[client#{i}]\\nuser=#{db_user}\\npassword=#{db_password}\\n\\n"
-    end
-    print "Salvando arquivo de configuração no server: "
-    start_spinner
-    run("cd ~/ && echo -e \"#{mysqlpassfile_content}\" > .my.cnf && chmod 600 .my.cnf")
-    stop_spinner
-    puts "OK".green
-    set :databases, databases
+  task :prepare, :roles => [:db] do
+    
+    find_servers_for_task(current_task).each do |s|
+      servers = Server.select{ |server| server.connection.host == s.host.to_s }
+      mysqlpassfile_content = ""
+
+      servers.each do |server|
+        puts "Preparando para criar o arquivo de senhas do mysql no host #{server.connection.host.yellow}..."
+        db_name = server.database.database_name
+        puts "Informações do database #{db_name.yellow}"
+        db_user = Capistrano::CLI.ui.ask("Usuário do mysql: ")
+        db_password = Capistrano::CLI.password_prompt("Senha do mysql: ")
+        code = Digest::MD5.hexdigest db_name
+        mysqlpassfile_content << "[mysqldump#{code}]\\nuser=#{db_user}\\npassword=#{db_password}\\n\\n[client#{code}]\\nuser=#{db_user}\\npassword=#{db_password}\\n\\n"
+      end
+      
+      print "Salvando arquivo de configuração no server: "
+      start_spinner
+      run("cd ~/ && echo -e \"#{mysqlpassfile_content}\" > .my.cnf && chmod 600 .my.cnf", :hosts => s)
+      stop_spinner
+      puts "OK".green
+    end  
   end
   
-  task :check do
-    databases.each_with_index do |db, i|
-      print "Testando comunicação com o banco #{db.yellow}: "
-      start_spinner
-      database = capture("mysqlshow --defaults-group-suffix=#{i}")
-      stop_spinner
-      raise Capistrano::Error if database.index(db).nil?
-      puts "OK".green
+  task :check, :roles => [:db] do
+    
+    find_servers_for_task(current_task).each do |s|
+      servers = Server.select{ |server| server.connection.host == s.host.to_s }
+      servers.each do |server|
+        db_name = server.database.database_name
+        code = Digest::MD5.hexdigest db_name
+        print "Testando comunicação com o banco #{db_name.yellow}: "
+        start_spinner
+        run("mysqlshow --defaults-group-suffix=\"#{code}\" #{db_name}", :hosts => s)
+        stop_spinner
+        puts "OK".green
+      end
+      
     end
   end
 end
