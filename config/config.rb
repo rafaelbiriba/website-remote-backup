5629fbd1efb30ea8020981d43ead0ab8b306d4de
Rafael Biriba <biribarj@gmail.com>
2013-05-12 23:34:53 -0300

    implementando escolha do host de backup por environment variable

--- config/config.rb
+++ config/config.rb
@@ -3,4 +3,14 @@ Dir[File.dirname(__FILE__) + "/servers/*.yml"].each do |file|
   config << YAML.load_file(file)
 end
 
+env_host = ENV["HOST"]
+if env_host
+  selected_host = config.select { |s| s["connection"]["host"] == env_host }
+  if selected_host.empty?
+    raise "Host #{env_host} não encontado!"
+  else
+    config = selected_host
+  end
+end
+
 Server = hashes2ostruct(config)
